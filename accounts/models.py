cff503bd116de7a391558b5dddfd99cb81d8e3aa
5y0ung4 <qwerrr504@naver.com>
2025-07-02 22:53:48 +0900

    User 모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    #유저 아이디
+    user_id = models.CharField(max_length=12, unique=True)
+
+    #비밀번호
+    password = models.CharField(null=False, blank=False, max_length=128)
+
+    #성별
+    SEX_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('N', 'None')
+    ]
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, default='N')
+    
+    #출생연도
+    birth_year = models.PositiveIntegerField(default=2000, blank=False)
